0c6e8cb88dd3d7eb90de3a61f447208717becd98
safiabano <safiabano736@gmail.com>
2023-05-08 15:37:26 +0500

    js

---
+++ js/assets/js/app.js
@@ -0,0 +1,118 @@
+// var myName="anas"
+// let userInfo=prompt("how old are you?", "22");
+// if(userInfo>=18){
+//     console.log("you are hire");
+// }else{
+//     console.log("you are not allowed")
+// }
+// let lang =prompt("enter your language?");
+// if(lang === "html"){
+// alert("you are hired");
+// }else if(lang === "css"){
+//     alert("great");
+// }else{
+//     alert("no allow");
+// };
+// let lang =prompt("enter your language");
+// if(lang === "java"){
+//     console.log("welcome java");
+//  }else if(lang === "css"){
+//         console.log("ajao css");
+//  } else if(lang=== "ecma"){
+//    console.log("wow");
+//  }
+//  else{ console.log("not come");
+// }
+
+// let firstName ="hello";
+// let lastName ="world";
+// console.log(firstName + " " +lastName);
+// let lang = prompt("enter your lang");
+// let lang2=prompt("enter your lang2")
+// if(lang==="html"|| lang2 ==="css"){
+//     alert("you are hired")
+// }else if(lang ==="java"|| lang2 === "ecma"){
+//    alert("good")
+// }
+// else{
+//    alert("you are not hired")
+// }
+
+// let num1 =3;
+// let num2 =6;
+// console.log("---> "+ num1);
+// console.log("---> "+ num2);
+
+
+//let abc = 31
+
+
+
+// for(let i = 1 ; i <= 10 ; i++){
+// document.write(i+"<br>")
+// }
+
+
+
+// for(let i = 0 ;i <=20; i++){
+//     console.log(i)
+// }
+
+// let num = + prompt("enter your number");
+// if(num %3 === 0 && num %5 ===0){
+//     console.log("both 3 and 5");
+// }else if(num %3 ===0){
+//     console.log("only3");
+// }else if(num %5===0){
+//  console.log("only 5");
+// }
+// let num  = prompt("enter number")
+// if(num  %2===0 && num %8===0){
+//     console.log("2&8 both");
+// }else if(num %2===0 ){
+//     console.log("only2")
+// }else if(num %8===0 ){
+//     console.log("only 8")
+// }else{
+//     console.log("others")
+// }
+// let name =prompt("your name")
+// alert("welcome "+ name)
+
+//======================nested if ===========
+
+
+// let num = +prompt("enter your percentage");
+// let grade;
+// if(num >=0 && num <=100){
+//     if(num >=80){
+//         grade = " A+"
+//     }else if(num >=70){
+//         grade ="A"
+//     }else if(num >=60){
+//         grade ="B"
+//     }else if(num >=50){
+//         grade="c"
+//     }else{
+//         console.log("wrong input");
+//     }
+//   console.log(grade)  
+// }
+
+// let cities= ["karachi","lahore","islamabad","hyderabad","chicago"]
+
+// alert("welcome to "+ cities[2]);
+
+
+// var mixedArray = [1, "Bob", "Now is", true];
+// alert(mixedArray[0]);
+
+//====================================
+// (())
+
+// prompt(typeof+ abc)
+
+
+if(age >65||age <21 && res === us){
+ console.log("you are allowed");
+}
